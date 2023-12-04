922f1d2bb64f433dd6b611ab6a8670ed54088a64
camilo <camartinez@ceteris.mx>
2023-12-04 11:01:19 -0500

    Initial Commit

---
+++ Raices_Rurales/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Raices_Rurales
+//
+//  Created by Camilo Martínez Gualteros on 4/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Raices_Rurales/Raices_RuralesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Raices_RuralesApp.swift
+//  Raices_Rurales
+//
+//  Created by Camilo Martínez Gualteros on 4/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Raices_RuralesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
