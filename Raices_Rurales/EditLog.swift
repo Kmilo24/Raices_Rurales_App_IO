b1e2cddadcd1478e5b7b8d4b04821be40675b7e6
camilo <camartinez@ceteris.mx>
2023-12-04 19:59:17 -0500

    Inicio del desarrollo

---
+++ Raices_Rurales/EditLog.swift
@@ -0,0 +1,18 @@
+//
+//  EditLog.swift
+//  Raices_Rurales
+//
+//  Created by Camilo Martínez Gualteros on 4/12/23.
+//
+
+import SwiftUI
+
+struct EditLog: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditLog()
+}
